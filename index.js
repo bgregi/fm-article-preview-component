c1009f0d3c99049643ee2a87dd1bd86349bc0fb1
Bruno Geraldi <brunogeraldieng@gmail.com>
2021-07-19 20:57:05 -0300

    Mobile layout and JS done

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 // referenciar elementos footers
 // criar evento onclick no share button
 // mudar propriedade display ficar alternando
+
+function shareArticle() {
+    let profileFooter = document.getElementById("profile")
+    let shareFooter = document.getElementById("share")
+
+    if (profileFooter.style.display == "flex") {
+        profileFooter.style.display = "none"
+        shareFooter.style.display = "flex"        
+    } else {
+        profileFooter.style.display = "flex"
+        shareFooter.style.display = "none"
+    }    
+
+    console.log(profileFooter.style.maxHeight)
+}
+
+let profileButton = document.getElementById("profile-button")
+let shareButton = document.getElementById("share-button")
+
+profileButton.addEventListener("click", shareArticle)
+shareButton.addEventListener("click", shareArticle)
