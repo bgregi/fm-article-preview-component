6bedd62d0ec6b4214d3f0314fa16cdc6e56fbb79
Bruno Geraldi <brunogeraldieng@gmail.com>
2021-07-20 11:45:25 -0300

    Desktop button functionallity OK

--- index.js
+++ index.js
@@ -3,13 +3,36 @@
 function shareArticle() {
     let profileFooter = document.getElementById("profile")
     let shareFooter = document.getElementById("share")
+    let arrowDiv = document.getElementById("profile-button")
+    let arrowImage = document.getElementById("share-arrow")
 
-    if (profileFooter.style.display == "flex") {
-        profileFooter.style.display = "none"
-        shareFooter.style.display = "flex"        
+    if (window.screen.width < 800) {
+        if (profileFooter.style.display == "flex") {
+            profileFooter.style.display = "none"
+            shareFooter.style.display = "flex"
+        } else {
+            profileFooter.style.display = "flex"
+            shareFooter.style.display = "none"
+        }        
     } else {
-        profileFooter.style.display = "flex"
-        shareFooter.style.display = "none"
+        if (!shareFooter.style.display || shareFooter.style.display == "none") {
+            console.log(shareFooter.style.display)
+
+            shareFooter.style.display = "flex"
+            
+            arrowImage.src = "images/icon-share-white.svg"
+            arrowDiv.classList.toggle("profile-button")
+            arrowDiv.classList.toggle("share-button")
+            
+        } else {
+            console.log(toString(shareFooter.style.display))
+
+            shareFooter.style.display = "none"
+
+            arrowImage.src = "images/icon-share.svg"
+            arrowDiv.classList.toggle("profile-button")
+            arrowDiv.classList.toggle("share-button")
+        }
     }
 }
 
